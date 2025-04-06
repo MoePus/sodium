77469b90e5b185c36dbbc9cf45e8ff1b60f4051d
MoePus <547007249@qq.com>
2025-04-06 13:22:50 +0900

    update neoforge

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -2,7 +2,7 @@ import org.gradle.api.Project
 
 object BuildConfig {
     val MINECRAFT_VERSION: String = "1.21.1"
-    val NEOFORGE_VERSION: String = "21.1.90"
+    val NEOFORGE_VERSION: String = "21.1.120"
     val FABRIC_LOADER_VERSION: String = "0.16.9"
     val FABRIC_API_VERSION: String = "0.110.0+1.21.1"
 
