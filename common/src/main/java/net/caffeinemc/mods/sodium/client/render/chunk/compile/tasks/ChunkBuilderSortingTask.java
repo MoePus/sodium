50045642b7a81b3d1745f15950aa9f51894365bb
douira <douira100@gmail.com>
2025-06-23 04:09:26 +0200

    use sort task result size for task size estimation for consistency

--- common/src/main/java/net/caffeinemc/mods/sodium/client/render/chunk/compile/tasks/ChunkBuilderSortingTask.java
+++ common/src/main/java/net/caffeinemc/mods/sodium/client/render/chunk/compile/tasks/ChunkBuilderSortingTask.java
@@ -36,6 +36,6 @@ public class ChunkBuilderSortingTask extends ChunkBuilderTask<ChunkSortOutput> {
 
     @Override
     public long estimateTaskSizeWith(MeshTaskSizeEstimator estimator) {
-        return this.sorter.getQuadCount();
+        return this.sorter.getResultSize();
     }
 }
