ca328f257dd1ae7a5cb44e25ad63b3d6e4d53e4e
douira <douira@users.noreply.github.com>
2025-11-09 17:52:05 +0100

    Improved buffer size estimation leading to far fewer buffer allocations and much less resizing (#3313)

--- common/src/main/java/net/caffeinemc/mods/sodium/client/render/chunk/region/RenderRegionManager.java
+++ common/src/main/java/net/caffeinemc/mods/sodium/client/render/chunk/region/RenderRegionManager.java
@@ -20,10 +20,7 @@ import net.caffeinemc.mods.sodium.client.render.chunk.terrain.TerrainRenderPass;
 import net.caffeinemc.mods.sodium.client.render.chunk.translucent_sorting.data.SharedIndexSorter;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class RenderRegionManager {
     private final Long2ReferenceOpenHashMap<RenderRegion> regions = new Long2ReferenceOpenHashMap<>();
@@ -132,11 +129,12 @@ public class RenderRegionManager {
         }
 
         var resources = region.createResources(commandList);
+        var regionFillFractionInv = region.getFillFractionInv();
 
         if (!uploads.isEmpty()) {
             var arena = resources.getGeometryArena();
             boolean bufferChanged = arena.upload(commandList, uploads.stream()
-                    .map(upload -> upload.vertexUpload));
+                    .map(upload -> upload.vertexUpload), regionFillFractionInv);
 
             // If any of the buffers changed, the tessellation will need to be updated
             // Once invalidated the tessellation will be re-created on the next attempted use
@@ -157,7 +155,7 @@ public class RenderRegionManager {
         if (!indexUploads.isEmpty()) {
             var arena = resources.getIndexArena();
             indexBufferChanged = arena.upload(commandList, indexUploads.stream()
-                    .map(upload -> upload.indexBufferUpload));
+                    .map(upload -> upload.indexBufferUpload), regionFillFractionInv);
 
             for (PendingSectionIndexBufferUpload upload : indexUploads) {
                 var storage = region.createStorage(DefaultTerrainRenderPasses.TRANSLUCENT);
@@ -166,7 +164,7 @@ public class RenderRegionManager {
         }
 
         if (needsSharedIndexUpdate) {
-            indexBufferChanged |= translucentStorage.updateSharedIndexData(commandList, resources.getIndexArena());
+            indexBufferChanged |= translucentStorage.updateSharedIndexData(commandList, resources.getIndexArena(), regionFillFractionInv);
         }
 
         if (indexBufferChanged) {
